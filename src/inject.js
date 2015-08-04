9422baaa91c10193bac963ee21ad0aed8fe995b5
Morgante Pell <morgante.pell@morgante.net>
2015-08-04 00:37:37 -0400

    realtime, working version

--- src/inject.js
+++ src/inject.js
@@ -1,55 +1,84 @@
 (function() {
-	chrome.storage.sync.get("hn_banned", function(value) {
-		var users = value.hn_banned || {};
+	function init() {
+		chrome.storage.sync.get("hn_banned", function(value) {
+			var users = value.hn_banned || {};
 
-		function banUser(username) {
-			users[username] = {
-				username: username,
-				blocked: true,
-				timestamp: Date.now()
-			};
-			chrome.storage.sync.set({"hn_banned": users});
-		}
-
-		function unbanUser(username) {
-			delete users[username];
-			chrome.storage.sync.set({"hn_banned": users});
-		}
-
-		function handleItem(item) {
-			var username = item.querySelector("a").text;
-
-			var actor = document.createElement('a');
-			actor.href = "#";
-
-			if (users[username] !== undefined) {
-				item.querySelector(".comment").innerHTML = "[blocked]";
-
-				actor.innerHTML = "unblock";
-				actor.onclick = function() {
-					unbanUser(username);
-					return false;
+			function banUser(username) {
+				users[username] = {
+					username: username,
+					blocked: true,
+					timestamp: Date.now()
 				};
-			} else {
-				actor.innerHTML = "block";
-				actor.onclick = function() {
-					banUser(username);
-					return false;
-				};
+				chrome.storage.sync.set({"hn_banned": users});
 			}
 
-			item.querySelector(".comhead").appendChild(actor);
-		}
+			function unbanUser(username) {
+				delete users[username];
+				chrome.storage.sync.set({"hn_banned": users});
+			}
 
-		var comments = document.querySelectorAll(".default");
+			function listener(changes, namespace) {
+				chrome.storage.onChanged.removeListener(listener);
+				init();
+			}
 
-		console.log('comments', comments, users);
+			chrome.storage.onChanged.addListener(listener);
 
-		for (var i = 0; i < comments.length; ++i) {
-			handleItem(comments[i]);
-		}
+			function handleItem(item) {
+				var username = item.querySelector("a").text;
+				var contents = item.querySelector(".comment span");
 
-	});
+				var actor = item.querySelector(".actor");
 
+				if (actor !== null) {
+					actor.remove();
+					actor = null;
+				}
+
+				actor = document.createElement('a');
+				actor.href = "#";
+				actor.className = "actor";
+
+				var blockedMessage = item.querySelector(".blocked");
+
+				if (blockedMessage !== null) {
+					blockedMessage.remove();
+				}
+
+				if (users[username] !== undefined) {
+					contents.style.display = "none";
+
+					actor.innerHTML = "unblock";
+					actor.onclick = function() {
+						unbanUser(username);
+						return false;
+					};
+
+					var banned = document.createElement('span');
+					banned.innerHTML = "[blocked]";
+					banned.className = "blocked";
+
+					item.querySelector(".comment").appendChild(banned);
+				} else {
+					contents.style.display = "block";
+
+					actor.innerHTML = "block";
+					actor.onclick = function() {
+						banUser(username);
+						return false;
+					};
+				}
+
+				item.querySelector(".comhead").appendChild(actor);
+			}
+
+			var comments = document.querySelectorAll(".default");
+
+			for (var i = 0; i < comments.length; ++i) {
+				handleItem(comments[i]);
+			}
+		});
+	}
+
+	init();
 })();
-
